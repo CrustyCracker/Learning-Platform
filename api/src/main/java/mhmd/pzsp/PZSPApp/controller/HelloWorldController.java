ced67900cf375c32f1ea3d46d334b8ae862848a3
Daniel <danielkobialka23@gmail.com>
2022-11-20 20:10:09 +0100

    Changed response to JSON

--- api/src/main/java/mhmd/pzsp/PZSPApp/controller/HelloWorldController.java
+++ api/src/main/java/mhmd/pzsp/PZSPApp/controller/HelloWorldController.java
@@ -1,12 +1,14 @@
 package mhmd.pzsp.PZSPApp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloWorldController {
     @GetMapping("/start")
     public String helloWorld() {
-        return "Dzień dobry";
+        return "{\"msg\": \"Dzień dobry\" }";
     }
 }
