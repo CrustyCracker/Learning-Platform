d8831c9b87336ba1b7e6c771b3c2ca25df5388ce
HubSor <44928255+HubSor@users.noreply.github.com>
2022-12-09 20:41:18 +0100

    Naprawienie logowania i rejestracji.

--- api/src/main/java/mhmd/pzsp/PZSPApp/interfaces/IAccountService.java
+++ api/src/main/java/mhmd/pzsp/PZSPApp/interfaces/IAccountService.java
@@ -1,8 +1,8 @@
 package mhmd.pzsp.PZSPApp.interfaces;
 
 import mhmd.pzsp.PZSPApp.exceptions.BackendException;
-import mhmd.pzsp.PZSPApp.models.api.LoginRequest;
-import mhmd.pzsp.PZSPApp.models.api.RegisterRequest;
+import mhmd.pzsp.PZSPApp.models.api.requests.LoginRequest;
+import mhmd.pzsp.PZSPApp.models.api.requests.RegisterRequest;
 
 public interface IAccountService {
     boolean login(LoginRequest login) throws BackendException;
@@ -10,4 +10,8 @@ public interface IAccountService {
     boolean register(RegisterRequest register) throws BackendException;
 
     boolean validatePassword(String password) throws BackendException;
+
+    boolean validateMail(String mail) throws BackendException;
+
+    byte[] hashPassword(String password, byte[] salt) throws BackendException;
 }
