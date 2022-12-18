07c44791cd05f0c0ca74f066b7a3371d4350124d
mkrakows <matkrakowski1@gmail.com>
2022-12-18 15:07:20 +0100

    login reginster

--- platform/src/components/LoginForm.tsx
+++ platform/src/components/LoginForm.tsx
@@ -30,34 +30,34 @@ export function LoginForm(props: LoginFormProps) {
 
     const cardStyle = {
         margin: "5% 0",
-        minHeight: '600px',
-        minWidth: '400px',
+        minHeight: '420px',
+        minWidth: '420px',
         alignItems: 'center',
         justifyContent: 'center',
         padding: '5%'
     }
     // pzsp2 error handling i walidacja
+    // pzsp2 max długość wpisywanego tesktu
     return (<> <link rel="stylesheet" href="../style/login.css"/>
     
         <form onSubmit={handleSubmit}>
-        <div className="card text-white bg-dark" style={cardStyle}>
-            <label style={{margin: 0, textAlign: 'start'}}>
-                <small>Nazwa użytkownika</small>
-                <input className="form-control" type="text" name="username" style={{marginBottom: "10%"}} maxLength={100} required onChange={(e) => {
-                    setCredentials({...credentials, username: e.target.value})
-                }} />
-            </label>
-            <label style={{margin: 0, textAlign: 'start'}}>
-                <small>Hasło</small>
-                <input className="form-control" type="text" name="username" style={{marginBottom: "10%"}} maxLength={100} required onChange={(e) => {
-                    setCredentials({...credentials, username: e.target.value})
-                }} />
-            </label>
-            <div style={{justifyContent: "center", marginTop: "2%"}}>
-            </div>
-            <button type="submit" onClick={handleSubmit} className="btn btn-outline-info">Zaloguj się</button>
-
-           <small>Nie masz konta? <Link to="/register">Załóż konto</Link></small> 
+            <div className="card text-white bg-dark" style={cardStyle}>
+                <label style={{margin: 0, textAlign: 'start', minWidth: "80%"}}>
+                    <small>Nazwa użytkownika</small>
+                    <input className="form-control" type="text" name="username" style={{marginBottom: "4%"}} maxLength={100} required onChange={(e) => {
+                        setCredentials({...credentials, username: e.target.value})
+                    }} />
+                </label>
+                <label style={{margin: 0, textAlign: 'start', minWidth: "80%"}}>
+                    <small>Hasło</small>
+                    <input className="form-control" type="text" name="password" style={{marginBottom: "4%"}} maxLength={100} required onChange={(e) => {
+                        setCredentials({...credentials, password: e.target.value})
+                    }} />
+                </label>
+                <div style={{justifyContent: "center", marginTop: "4%"}}></div>
+                <button type="submit" onClick={handleSubmit} className="btn btn-outline-info">Zaloguj się</button>
+                <div style={{justifyContent: "center", marginTop: "4%"}}></div>
+                <small>Nie masz konta? <Link to="/register">Załóż konto</Link></small> 
             </div>
         </form>
     </>)
