cb3c2a15b4b266b720b552aad89e321553409940
Maciej Tymoftyjewicz <01158968@pw.edu.pl>
2023-01-07 23:25:13 +0100

    Adding group in new card form - front

--- platform/src/components/NewCardForm.tsx
+++ platform/src/components/NewCardForm.tsx
@@ -1,9 +1,11 @@
-import React, {FormEvent, useState} from "react";
+import React, {FormEvent, useEffect, useState} from "react";
 import {Requests} from "../requests/Requests";
 import {CardResponse, NewCard} from "../types/Cards";
 import {ErrorResponse} from "../types/ErrorResponse";
 import {useNavigate} from 'react-router-dom';
 import '../style/cardForm.css';
+import {GroupResponse} from "../types/Groups";
+import {Link} from "react-router-dom";
 
 interface CardFormProps {
     onSuccess: (response: CardResponse) => void,
@@ -12,7 +14,30 @@ interface CardFormProps {
 
 export function NewCardForm(props: CardFormProps) {
     const [newCard, setNewCard] = useState({} as NewCard);
+    const [groups, setGroups] = useState<GroupResponse[]>([]);
+    const [addedgroups, setAddedGroups] = useState<GroupResponse[]>([]);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        Requests.myGroups().then(res => {
+            if (res.err) {
+                setGroups([]);
+                props.onError(res.err);
+            }
+            else if (res.res){
+                setGroups(res.res);
+            }
+        });
+    }, [])
+
+    const updateGroups = (group: GroupResponse) => {
+        if (!addedgroups.some(group_ => group === group_))
+            setAddedGroups(addedgroups.concat(group))
+        let groupIds = addedgroups.map(group => group.id)
+        setNewCard({...newCard, groupsIds: groupIds})
+    }
+
+
 
     const AddCard = (e: FormEvent) => {
         e.preventDefault();
@@ -91,25 +116,48 @@ export function NewCardForm(props: CardFormProps) {
                     </div>
                 </div>
             </div>
-        <div className="form-check pzsp2-cardform-pubcheck">
-            <input className="form-check-input pzsp2-pubcheck-input" type="checkbox" name="isPublic"
-                   checked={newCard.isPublic} id="isPublic" onChange={(e) => {
-                                     if (e.target.value) setNewCard({...newCard, isPublic: e.target.checked})}}/>
-                <label className="form-check-label" htmlFor="isPublic">
-                    Publiczna
-                </label>
-        </div>
-        <form>
-        <div className="row">
-            <div className="btn-group pzsp2-cardform-buttons" role="group">
-                <div className="pzsp2-cardform-add-button">
-                    <button type="submit" onClick={AddCard} className="btn btn-outline-success">Dodaj fiszkę</button>
+            <div className="form-check pzsp2-cardform-pubcheck">
+                <input className="form-check-input pzsp2-pubcheck-input" type="checkbox" name="isPublic"
+                       checked={newCard.isPublic} id="isPublic" onChange={(e) => {
+                                         if (e.target.value) setNewCard({...newCard, isPublic: e.target.checked})}}/>
+                    <label className="form-check-label" htmlFor="isPublic">
+                        Publiczna
+                    </label>
+            </div>
+            <div className="pzsp2-cardform-group-dropdown">
+                <div className="btn-group">
+                    <button type="button" className="btn btn-secondary btn-lg">Dodaj grupę</button>
+                    <button type="button" className="btn btn-secondary btn-lg dropdown-toggle dropdown-toggle-split"
+                            data-bs-toggle="dropdown"/>
+                    <ul className="dropdown-menu dropdown-menu-dark">
+                        {groups && groups.map(group => {
+                            return <li key={group.id} ><div className="dropdown-item pzsp2-cardform-dropdown-item"
+                                                            onClick={() => updateGroups(group)}>{group.name}</div></li>})}
+                        <li>
+                            <hr className="dropdown-divider"/>
+                        </li>
+                        <li><Link className="dropdown-item" to="/groups/new"><i className="bi bi-plus-circle"/> Dodaj do grupy</Link></li>
+                    </ul>
                 </div>
-                <div className="pzsp2-cardform-addplus-button">
-                    <button type="submit" onClick={AddCardAndNext} className="btn btn-outline-success">Dodaj fiszkę + kolejną</button>
+                <div>
+                    <p className="pzsp2-cardform-groups-header">
+                        Grupy: {addedgroups && addedgroups.map(group => {
+                            return <span className="pzsp2-cardform-groups">{`${group.name}, `}</span>}
+                    )}
+                    </p>
                 </div>
             </div>
-        </div>
+        <form>
+            <div className="row">
+                <div className="btn-group pzsp2-cardform-buttons" role="group">
+                    <div className="pzsp2-cardform-add-button">
+                        <button type="submit" onClick={AddCard} className="btn btn-outline-success">Dodaj fiszkę</button>
+                    </div>
+                    <div className="pzsp2-cardform-addplus-button">
+                        <button type="submit" onClick={AddCardAndNext} className="btn btn-outline-success">Dodaj fiszkę + kolejną</button>
+                    </div>
+                </div>
+            </div>
         </form>
     </div>)
 }
