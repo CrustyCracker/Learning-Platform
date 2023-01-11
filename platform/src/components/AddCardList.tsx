1cc47e256c46d97d046fe9c439fd149345d12425
Mateusz Łukasz Krakowski <01158769@pw.edu.pl>
2023-01-11 15:46:08 +0100

    Fe be dodawanie fiszek do grup od strony newGroup i editGroup

--- platform/src/components/AddCardList.tsx
+++ platform/src/components/AddCardList.tsx
@@ -1,32 +1,33 @@
-import React, {FormEvent, useEffect, useState} from "react";
-import {CardResponse} from "../types/Cards";
+import React, { FormEvent, useEffect, useState } from "react";
+import { CardResponse } from "../types/Cards";
 import { TagsInput } from "react-tag-input-component";
 import '../style/cardList.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { isPublicToString } from "../helpers/NameHelpers";
 
-interface CardListProps {
+interface AddCardListProps {
     cards: CardResponse[],
     pub: boolean,
-    learn?: boolean
-
+    add?: boolean,
+    learn?: boolean,
+    groupId?: String
 }
 
-export function CardList(props: CardListProps) {
+export function AddCardList(props: AddCardListProps) {
     const [currCards, setCurrCards] = useState<CardResponse[]>([]);
-    const [tags, ] = useState<string[]>([]);
+    const [tags,] = useState<string[]>([]);
 
     useEffect(() => {
         setCurrCards(props.cards)
     }, [props])
 
     const filterByTags = (tags: string[]) => {
-        if(tags.length) {
-            let newCards:CardResponse[] = [];
+        if (tags.length) {
+            let newCards: CardResponse[] = [];
 
             currCards.forEach((card) => {
                 tags.forEach((tag) => {
-                    if(card.tagNames.some(x => x.toLowerCase() === tag.toLowerCase())){
+                    if (card.tagNames.some(x => x.toLowerCase() === tag.toLowerCase())) {
                         newCards.push(card)
                     }
                 })
@@ -51,15 +52,15 @@ export function CardList(props: CardListProps) {
     //pzsp2 - dodać animację dla każdego rzędu w tabeli, oddzielny fadeIn z delayem np 100ms
     return <>
         <div className="pzsp2-cardlist-cont">
-        <TagsInput
-            value={tags}
-            onChange={e => filterByTags(e || [])}
-            name="tagi"
-            placeHolder="Dodaj tag..."
-        />
-        <p className="pzsp2-cardlist-tagsearch">
-            <em>Wyszukaj po tagu... Kilknij ENTER, aby dodać nowy tag.</em>
-        </p>
+            <TagsInput
+                value={tags}
+                onChange={e => filterByTags(e || [])}
+                name="tagi"
+                placeHolder="Dodaj tag..."
+            />
+            <p className="pzsp2-cardlist-tagsearch">
+                <em>Wyszukaj po tagu... Kilknij ENTER, aby dodać nowy tag.</em>
+            </p>
         </div>
         <div className="pzsp2-cardlist-cont">
             <div className="row pzsp2-cardlist-row-buttons">
@@ -78,6 +79,7 @@ export function CardList(props: CardListProps) {
         <table className={"table table-hover table-light pzsp2-cardlist-table"}>
             <thead>
                 <tr>
+                    <th className="pzsp2-cardlist-table-tag">Checked?</th> {/*//pzsp2 - zmienić className*/}
                     <th className="pzsp2-cardlist-table-que">Pytanie</th>
                     <th className="pzsp2-cardlist-table-grp">Grupy</th>
                     <th className="pzsp2-cardlist-table-tag">Tagi</th>
@@ -89,6 +91,7 @@ export function CardList(props: CardListProps) {
             <tbody>
                 {currCards && currCards.map(card => {
                     return <tr key={card.id}>
+                        {props.groupId && <td><input type="checkbox" /></td>}
                         <td className="pzsp2-cardlist-td-wrap">{card.question}</td>
                         <td className="pzsp2-cardlist-td-wrap">{card.groupNames}</td>
                         <td className="pzsp2-cardlist-td-wrap">{card.tagNames}</td>
--- platform/src/pages/NewGroupPage.tsx
+++ platform/src/pages/NewGroupPage.tsx
@@ -1,17 +1,32 @@
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 import '../style/App.css';
 import '../style/groupForm.css';
-import {ErrorAndInfo} from "../components/ErrorAndInfo";
-import {NewGroupForm} from "../components/NewGroupForm";
+import { ErrorAndInfo } from "../components/ErrorAndInfo";
+import { NewGroupForm } from "../components/NewGroupForm";
 import Layout from "../components/Layout/Layout";
-import {SecurityHelper} from "../helpers/SecurityHelper";
-import {Navigate} from "react-router-dom";
-import {Helmet} from "react-helmet";
+import { SecurityHelper } from "../helpers/SecurityHelper";
+import { Navigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { CardResponse } from '../types/Cards';
+import { Requests } from '../requests/Requests';
 
 export default function NewGroupPage() {
     const [error, setError] = useState("");
+    const [cards, setCards] = useState<CardResponse[]>([]);
 
-    if(!SecurityHelper.amILogged())
+    useEffect(() => {
+        Requests.allCards().then(res => {
+            if (res.err) {
+                setCards([]);
+                setError(res.err.userMessage);
+            }
+            else if (res.res) {
+                setCards(res.res);
+            }
+        });
+    }, [])
+
+    if (!SecurityHelper.amILogged())
         return <Navigate to="/login" />
 
     return <>
@@ -21,7 +36,7 @@ export default function NewGroupPage() {
         <Layout>
             <div className="App container-fluid pzsp2-groupform-page-cont">
                 <ErrorAndInfo errorMsg={error} infoMsg={""} />
-                <NewGroupForm onSuccess={() => {}} onError={(res) => setError(res.userMessage)}/>
+                <NewGroupForm onSuccess={() => { }} onError={(res) => setError(res.userMessage)} cards={cards} />
             </div>
         </Layout>
     </>;
