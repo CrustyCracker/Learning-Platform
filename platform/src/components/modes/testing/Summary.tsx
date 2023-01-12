debbed3a6b7cd5d467992d2b611493f46ef22008
HubSor <44928255+HubSor@users.noreply.github.com>
2023-01-12 19:16:37 +0100

    Dodanie podsumowania na oddzielnej stronie.

--- platform/src/components/modes/testing/Summary.tsx
+++ platform/src/components/modes/testing/Summary.tsx
@@ -1,8 +1,14 @@
 export function Summary(props: {score: number, outOf: number}) {
 
     return <div className="App pzsp2-login-page-cont">
-        <h2 className="pzsp2-cardlist-title">Ukończono test!</h2>
-        <h3>Twój wynik to {props.score} / {props.outOf}</h3>
-        <h3>Brawo!</h3>
+        <div className="row pzsp2-login-row">
+            <div className="col-lg-3 col-md-6 col-sm-12">
+                <div className="card text-black bg-light pzsp2-login-card">
+                    <h2 className="pzsp2-cardlist-title">Ukończono test!</h2>
+                    <h3>Twój wynik to {props.score} / {props.outOf}</h3>
+                    <h3>Brawo!</h3>
+                </div>
+            </div>
+        </div>
     </div>
 }
--- platform/src/components/modes/testing/TestingMode.tsx
+++ platform/src/components/modes/testing/TestingMode.tsx
@@ -1,7 +1,7 @@
 import {CardResponse} from "../../../types/Cards";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import CardInTesting from "./CardInTesting";
-import {Summary} from "./Summary";
+import {useNavigate} from "react-router-dom";
 
 type TestingModeProps = {
     cards: CardResponse[]
@@ -11,7 +11,16 @@ export default function TestingMode(props: TestingModeProps) {
     const [currCardIndex, setCurrCardIndex] = useState(0);
     const [answerValue, setAnswerValue] = useState(0);
     const [answered, setAnswered] = useState(false);
+    const navigate = useNavigate();
+
     const card = props.cards.at(currCardIndex);
+
+    useEffect(() => {
+        if (props.cards && currCardIndex > 0 && currCardIndex === props.cards.length)
+            navigate("/summary", {
+                state: {score: answerValue, outOf: props.cards.length}
+            })
+    }, [currCardIndex, navigate])
 
     const addLastValue = (num: number) => {
         setAnswerValue(answerValue + num);
@@ -20,16 +29,22 @@ export default function TestingMode(props: TestingModeProps) {
 
     return <>
         {card && <CardInTesting card={card} onAnswer={addLastValue} />}
-        {!card && <Summary score={answerValue} outOf={props.cards.length} />}
-        {currCardIndex < props.cards.length && answered && <div className ="pzsp2-next-testing"
-            onClick={() => {
-                setCurrCardIndex(currCardIndex + 1)
-                setAnswered(false);
-            }}>
-                <button type="button" className="btn btn-success">
-                    {currCardIndex < props.cards.length - 1 ? "Następna" : "Podsumowanie"}
-                </button>
+        <div className="container-fluid pzsp2-browse-cont">
+            <div className="pzsp2-browse-buttons">
+                {currCardIndex < props.cards.length && answered && <form className ="pzsp2-next-button"
+                    onClick={() => {
+                        setCurrCardIndex(currCardIndex + 1)
+                        setAnswered(false);
+                    }}>
+                        <button type="button" className="btn btn-success">
+                            {currCardIndex < props.cards.length - 1 ? "Następna" : "Podsumowanie"}
+                        </button>
+                    </form>
+                }
+                <div className="pzsp2-browse-count">
+                    {currCardIndex + 1}/{props.cards.length}
+                </div>
             </div>
-        }
+        </div>
     </>
 }
